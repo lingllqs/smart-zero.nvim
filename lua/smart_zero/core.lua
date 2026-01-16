613d6405762024cac986e30f0493418af29a77ef
lingllqs <lingllqs@163.com>
2026-01-16 23:36:21 +0800

    fix bug

--- lua/smart_zero/core.lua
+++ lua/smart_zero/core.lua
@@ -5,13 +5,13 @@ function M.smart_move()
 
 	-- 光标位置（row: 1-based, col: 0-based byte）
 	local row, col = unpack(vim.api.nvim_win_get_cursor(win))
-	local line = vim.api.nvim_get_current_line()
+	local line = vim.api.nvim_get_current_line() -- 获取行内容
 
 	-- 当前显示列（1-based）
 	local vcol = vim.fn.wincol()
 
 	-- 行显示宽度（$ 的真实位置）
-	local width = vim.fn.strdisplaywidth(line)
+	local width = vim.fn.strdisplaywidth(line) -- cell width
 
 	-- 第一个非空白字符（byte index, 0-based）
 	local indent = vim.fn.match(line, "\\S")
@@ -21,14 +21,18 @@ function M.smart_move()
 
 	local target_col
 
-	if vcol == 1 then
+	if vcol == 1 then -- 当前光标在第一列
 		if indent > 0 then
 			target_col = indent
 		else
-			target_col = #line
+			target_col = width
 		end
 	elseif vcol < width then
-		target_col = #line
+        if vcol < indent then
+            target_col = indent
+        else
+            target_col = width
+        end
 	else
 		target_col = 0
 	end
